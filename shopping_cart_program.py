5b97ce62efe57fd9d89976e012e151708b590011
mariapan0330 <maria.pan0330@gmail.com>
2022-07-22 14:48:52 -0500

    first commit. project is almost done, minor tweaks from here

---
+++ shopping_cart_program.py
@@ -0,0 +1,223 @@
+# Build a shopping cart program with the following capabilities:
+
+# 1) Takes in an input
+# 2) Stores user input into a dictionary or list
+# 3) The User can add or delete items
+# 4) The User can see current shopping list
+# 5) The program Loops until user quits
+# 6) Upon quiting the program, prints out a receipt of the items with total and quantity.
+
+# from IPython.display import clear_output
+import os
+
+
+cart = {}
+# EXAMPLE cart = {"item_name": [quantity, price]}
+
+# ========================= ADD ========================= 
+def add():
+    os.system("cls")
+
+    print("\n================================ ADD ================================ ")
+
+    while True:
+        item = input("What would you like to add to your cart? ").lower()
+        if item == 'done':
+            show(True)
+            break
+        else:        
+            print(f"\tYou've selected {item.upper()}.")
+
+        quant = input(f"\nHow many would you like to add? ")
+        
+        if quant == 'done':
+            show(True)
+            break
+        
+        if quant.isdigit() == False:
+            print("\t!!! Invalid Input. Please enter a whole number. !!!")
+            continue
+
+        try:
+            price = cart[item][1]
+        except:
+            print(f"\tYou've added {item.upper()} x {quant}.")
+            while True:
+                price = input(f"\nWhat is the price of one {item}? $")
+
+                if price == 'done':
+                    show(True)
+                    break
+                
+                try:
+                    price = "{:.2f}".format(float(price))
+                except:
+                    print("\t!!! Invalid Input. Please enter a number. !!!")
+                    continue
+                else:
+                    break
+        
+        if price == 'done':
+            break
+
+        print(f"\tYou've added {item.upper()} x {quant} at ${price} each.")
+        if item in cart: 
+            quant = int(cart[item][0]) + int(quant)
+            print(f"\tThere are now {item.upper()} x {quant} in your cart.")
+
+        cart[item] = [quant, price]
+        main()
+        break
+
+
+
+# ========================= REMOVE ========================= 
+def remove():
+    os.system("cls")
+    print("\n============================== REMOVE =============================== ")
+    while True:
+        if len(cart) == 0:
+            print("Cart is empty. Nothing to remove.")
+            main()
+            break
+
+        item = input("What item would you like to remove? \n(Enter an item or type ALL to remove all items from your cart): ").lower()
+        if item == 'done':
+            show(True)
+            break
+
+        if item == 'all':
+            while True:
+                really_clear = input("Are you sure you want to remove all items from your cart? This action cannot be undone. YES/NO: ").lower()
+                if really_clear == 'yes':
+                    cart.clear()
+                    print("\tYou have removed all items from your cart.")
+                    break
+                elif really_clear == 'no':
+                    break
+                else:
+                    print("\t!!! Invalid Input. !!!")
+                    continue
+        try:
+            if really_clear == 'yes':
+                break
+        except:
+            pass
+
+        if item in cart:
+            if cart[item][0] == 1:
+                print(f"\t{item.upper()} removed.")
+
+            quant = input(f"\nHow many {item.upper()} would you like to remove?" +
+                f"\n(Enter a whole number or type ALL to remove all {item.upper()} from your cart): ").lower()
+            if quant.isdigit() == False and quant != 'all':
+                print(f"\t!!! Invalid Input. Please enter a whole number or type ALL to remove all {item.upper()}. !!!")
+                continue
+            elif quant == 'all':
+                quant = int(cart[item][0])
+                print(f"\tRemoving all {quant} {item.upper()} from your cart.")
+            else:
+                print(f"\t{item.upper()} x {quant} has been removed from your cart.")
+            
+            quant = int(cart[item][0]) - int(quant)
+            if int(quant) <= 0:
+                del cart[item]
+                quant = 0
+            else:
+                cart[item][0] = quant
+            print(f"\tThere are now {item.upper()} x {quant} in your cart.")
+            main()
+            break
+        else:
+            print("\tThat item is not in your cart.")
+            main()
+            break
+
+
+# ========================= DONE ========================= 
+def done():
+    while True:
+        really_done = input("Are you sure you are done? You will not be able to modify your cart. YES/NO: ").lower()
+
+        if really_done == 'yes':
+            os.system("cls")
+            print('\n\n*********************************************************************')
+            print("Thanks for using Shopping Cart! Here's a review of what's in your cart:", end='')
+            show(True)
+            break
+        elif really_done == 'no':
+            break
+        else:
+            print("\t!!! Invalid Input. !!!")
+            continue
+
+
+# ========================= SHOW ========================= 
+def show(done=False):
+    os.system("cls")
+    print("\n============================= YOUR CART =============================")
+
+    if cart:
+        print(f"You have {len(cart)} items in your cart.")
+        total = 0
+        print(f"\tProduct\t\tQty\t\tUnit\t\tPrice")
+        print('---------------------------------------------------------------------')
+        for item, val in cart.items():
+            f_item = item
+            if len(f_item) > 6:
+                f_item = (item + '\n\t' + '-->')
+            price = '{:.2f}'.format(float(val[0])*float(val[1]))
+            print(f"\t{f_item.upper()}\t\t{val[0]}\t\t${val[1]}\t\t${price}")
+            total += float(price)
+        print("---------------------------------------------------------------------")
+        print(f"Your total is ${'{:.2f}'.format(total)}")
+
+    else:
+        print("There are no items in your cart.")
+
+    if done == False:
+        main()
+    else:
+        print("---------------------------------------------------------------------")
+
+# ========================= START ========================= 
+def start():
+    """ Starts the shopping cart program. 
+    Follow printed instructions or type help(command_name) for help. """
+    os.system("cls")
+    print("\n=========================== SHOPPING CART =========================== ")
+
+    print("Welcome to the Shopping Cart.")
+    print("Enter a command to get started: \n\tADD = Add item(s). \n\tREMOVE = Remove item(s)." + 
+         "\n\tSHOW = Show the items in your cart." +
+          "\n\tDONE = Type DONE at any time to exit the program and see a summary of your cart.")
+    main()
+
+# ========================= MAIN ========================= 
+def main():
+    while True:
+        print("---------------------------------------------------------------------")
+        inp = input("What would you like to do? (ADD / REMOVE / SHOW / DONE): ").lower()
+        if inp not in {'add', 'remove', 'show', 'done'}:
+            print("Invalid Input.")
+            continue
+        
+        if inp == 'done':
+            show(True)
+            break
+        
+        if inp == 'add':
+            add()
+            break
+        
+        if inp == 'show':
+            show()
+            break
+        
+        if inp == 'remove':
+            remove()
+            break
+    
+
+
+start()
